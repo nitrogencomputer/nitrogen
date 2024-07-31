2dbef5658046a0fe128113cb151e19f73a847d5c
Jim <jimsamuel50@gmail.com>
2024-07-31 19:15:19 +0100

    task:Node operations on existent verifications

--- src/block/block.cc
+++ src/block/block.cc
@@ -1,20 +1,45 @@
 #include "block.h"
 #include <stdexcept>
 
-template<typename T>
-BlockOps<T>::BlockOps(){}
+template <typename T>
+BlockOps<T>::BlockOps() {}
 
-template<typename T>
-std::string BlockOps<T>::create_block_header(int block_id, time_t transaction_time, std::size_t tx_amount){}
+template <typename T>
+bool block_node_value_exists(Node *genesis_block, std::size_t balance)
+{
+    if (genesis_block->balance <= 0 && genesis_block->preceding_node == nullptr)
+        return false;
+    Node *current = genesis_block;
+    while (current != nullptr)
+        if (current->balance == balance)
+            return true;
+    current = current->preceding_node;
+    return false;
+}
 
-template<typename T>
-BlockOps<T> BlockOps<T>::create_new_block(Blockstructure bs){
-    BlockOps* new_block_ops = new BlockOps();
+template <typename T>
+bool block_node_exists(Node *genesis_block, Node *target)
+{
+    Node *current = genesis_block;
+    while (current != nullptr)
+        if (current == target)
+            return true;
+    current = current->preceding_node;
+    return false;
+}
+
+template <typename T>
+BlockOps<T> BlockOps<T>::create_new_transaction(Node *sender, Node *receiver, std::size_t transaction_amount)
+{
+}
+
+template <typename T>
+BlockOps<T> BlockOps<T>::create_new_blockops(Blockstructure bs)
+{
+    BlockOps *new_block_ops = new BlockOps();
     std::forward_list<Blockstructure> new_block_store;
 
-    if(bs.tx_amount <= 0 
-        || bs.block_tx_details.sender == nullptr
-        || bs.block_tx_details.receiver == nullptr) 
+    if (bs.tx_amount <= 0 || bs.block_tx_details.sender == nullptr || bs.block_tx_details.receiver == nullptr)
         throw std::runtime_error("invalid transaction");
 
     Blockstructure block_structure;
@@ -24,15 +49,22 @@ BlockOps<T> BlockOps<T>::create_new_block(Blockstructure bs){
     block_structure.block_header = bs.block_header;
     block_structure.block_tx_details = bs.block_tx_details;
 
-    if(block_structure.block_tx_details.tx_hash.empty())
+    if (block_structure.block_tx_details.tx_hash.empty())
         throw std::runtime_error("no transaction hash");
-    if(block_structure.block_tx_details.sender->balance <= 0 )
+    if (block_structure.block_tx_details.sender->balance <= 0)
         throw std::runtime_error("zero or negative balance");
-    if(block_structure.block_tx_details.sender->balance >= 100)
+    if (block_structure.block_tx_details.sender->balance >= 100)
         throw std::runtime_error("balance manipulation");
-    
+
     new_block_store.push_front(block_structure);
     new_block_ops->data = new_block_store;
     free(new_block_ops);
     return new_block_ops;
+}
+
+template <typename T> 
+Node* create_new_block(BlockOps<T>& blockops)
+{
+    Node* new_block_node = new Node();
+    
 }
--- src/block/block.h
+++ src/block/block.h
@@ -1,39 +1,44 @@
 #include <iostream>
 #include <memory>
 #include <atomic>
-#include <vector> 
+#include <vector>
 #include <string>
 #include <mutex>
 #include <cstddef>
 #include <forward_list>
 
-typedef struct Node {
+typedef struct Node
+{
     std::size_t balance;
-    Node* preceding_node;
-}Node;
+    Node *preceding_node;
+} Node;
 
-typedef struct BlockTxDetails {
-    Node* sender;
-    Node* receiver;
+typedef struct BlockTxDetails
+{
+    Node *sender;
+    Node *receiver;
     std::string tx_hash;
-}BlockTxDetails;
+} BlockTxDetails;
 
-typedef struct Blockstructure {
+typedef struct Blockstructure
+{
     int block_id;
     time_t transaction_time;
     std::size_t tx_amount;
     std::string block_header;
     BlockTxDetails block_tx_details;
-}Blockstructure;
+} Blockstructure;
 
-typedef struct Block{
+typedef struct Block
+{
     std::size_t block_hash;
-    Block* next_block_node;
-}Block;
+    Block *next_block_node;
+} Block;
 
-template<typename T> 
-class BlockOps{
-    private:
+template <typename T>
+class BlockOps
+{
+private:
     Blockstructure blockstructure;
     std::string prev_hash;
     std::string nonce;
@@ -45,12 +50,23 @@ class BlockOps{
     std::vector<T> genesis;
     std::mutex block_mutex;
 
-    public: 
+public:
     BlockOps();
-    BlockOps(const BlockOps& block);
-    std::string create_block_header(int block_id, time_t transaction_time, std::size_t tx_amount);
-    BlockOps create_new_block(Blockstructure blockstructure);
-    friend std::ostream& operator<<(std::ostream& stream, const BlockOps& block);
-    friend std::istream& operator>>(std::istream& stream, const BlockOps& block);
-    char& operator[](unsigned int index){return blocks[index];}
+    BlockOps(const BlockOps &block);
+    
+    Node* create_new_block(BlockOps& blockops);
+    bool block_node_value_exists(Node* genesis_block, std::size_t balance);
+    bool block_node_exists(Node* genesis_block, Node* target);
+
+    BlockOps create_new_blockops(Blockstructure blockstructure);
+    BlockOps create_new_transaction(Node* sender, Node* receiver, std::size_t transaction_amount);
+   
+    friend std::ostream &operator<<(std::ostream &stream, const BlockOps &blockops)
+    {
+        stream << blockops.blockstructure.block_id << blockops.blockstructure.block_header << blockops.blockstructure.block_tx_details;
+    }
+    char &operator[](unsigned int index)
+    {
+        return blocks[index];
+    }
 };
